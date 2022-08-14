1b9b89205d3de04b1ab04a9f503d595823b06c30
JuliaCooper60 <104178146+JuliaCooper60@users.noreply.github.com>
2022-08-14 19:14:28 +1000

    Update files for review
    Level 1 completed and issues with filtering for level 2

---
+++ UFO-level-1/static/js/app.js
@@ -0,0 +1,99 @@
+
+
+// // from data.js
+var tableData = data;
+// console.log(tableData);
+
+// // Get a reference to the table body - table elements variables-
+var tbody = d3.select("tbody");
+
+// Console.log the ufoSighting data from data.js
+
+console.log(data);
+
+// Step 1: Loop Through `data` and console.log each UFO sighting object
+tableData.forEach(function(ufoSighting) {
+    console.log(ufoSighting);
+
+// Step 2:  Use d3 to append one table row `tr` for each UFO Sighting object
+    
+var row = tbody.append("tr");
+
+// Step 3: Use `Object.entries` to console.log each UFO Sighting value
+    Object.entries(ufoSighting).forEach(function([key, value]) {
+      console.log(key, value);
+      // Append a cell to the row for each value
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+
+//   // Select the button
+var button = d3.select("#filter-btn");
+button.on("click", function() {
+// select data to be filered      
+tbody.html("");
+
+        // Select the input date get the raw HTML nodes
+        var inputElement = d3.select("#datetime");
+        // Get the value property of the input date, City, State, shape, Duration and Comments
+        var inputValue = inputElement.property("value");
+        // console.log input value
+        console.log(inputValue);
+        // Filter Data with datetime equal to input value
+        var filteredData = tableData.filter(sighting => sighting.datetime === inputValue)
+        //console.log filter values
+        console.log(filteredData);
+
+        filteredData.forEach(function(selections) {
+        console.log(selections);
+    // Append one table row `tr` for each UFO Sighting object
+        var row = tbody.append("tr");
+    // Use `Object.entries` to console.log each UFO Sighting value
+        Object.entries(selections).forEach(function([key, value]) {
+            console.log(key, value);
+        // Append a cell to the row for each value
+            var cell = row.append("td");
+            cell.text(value);
+         });
+        });
+        });
+//   // Creat filter button to clear previous filters 
+var button = d3.select("#clear-filter-btn");
+button.on("click", function() {
+// select data to be filered      
+tbody.html("");
+
+// To clear filtger we need to Re-loop Through `data` and console.log each UFO sighting object
+tableData.forEach(function(ufoSighting) {
+    console.log(ufoSighting);
+
+// re-append one table row `tr` for each UFO Sighting object
+    
+var row = tbody.append("tr");
+
+//recreatges the full list 
+    Object.entries(ufoSighting).forEach(function([key, value]) {
+      console.log(key, value);
+      // Append a cell to the row for each value
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+  // Step 1: Loop Through `data` and console.log each UFO sighting object
+tableData.forEach(function(ufoSighting) {
+    console.log(ufoSighting);
+
+// Step 2:  Use d3 to append one table row `tr` for each UFO Sighting object
+    
+var row = tbody.append("tr");
+
+// Step 3: Use `Object.entries` to console.log each UFO Sighting value
+    Object.entries(ufoSighting).forEach(function([key, value]) {
+      console.log(key, value);
+      // Append a cell to the row for each value
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+});
--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -1,4 +1,66 @@
 // from data.js
-var tableData = data;
+const tableData = data;
+// get table references
+var tbody = d3.select("tbody");
+function buildTable(tableData) {
+  // First, clear out any existing data
+  tbody.html("");
+// Step 1: Loop Through `data` and console.log each UFO sighting object
+tableData.forEach(function(ufoSighting) {
+    console.log(ufoSighting);
 
-// YOUR CODE HERE!
+// Step 2:  Use d3 to append one table row `tr` for each UFO Sighting object
+    
+var row = tbody.append("tr");
+
+// Step 3: Use `Object.entries` to console.log each UFO Sighting value
+    Object.entries(ufoSighting).forEach(function([key, value]) {
+      console.log(key, value);
+      // Append a cell to the row for each value
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+}
+
+// 1. Create a variable to keep track of all the filters as an object.
+var filters = {};
+
+// 3. Create a function that updates the filters. 
+
+function updateFilters() {
+
+  // 4a. Create a variable that saves the element that was changed using d3.select().
+var element = d3.select(this);
+  // 4b. Save the value that was changed as a variable.
+var value = element.property("value");
+  // 4c. Save the id of the filter that was changed as a variable.
+var id = element.attr("id");
+
+  // 5. If a filter value was entered then add that filterId and value
+if (element){filters[id] = value}
+  // to the filters list. Otherwise, clear that filter from the filters object.
+else {delete filters[id]}
+  // 6. Call function to apply all filters and rebuild the table
+  filterTable();
+}
+
+// 7. Create a function that filters the table when data is entered.
+function filterTable() {
+
+  // 8. Set the filtered data to the tableData.
+ var filterTable = tableData;
+  // 9. Loop through all of the filters and keep any data that
+  // matches the filter values
+  Object.entries(filters).forEach (([key, value]) =>{
+    filterTable = filterTable.filter (row => row [key]=== value);
+   });
+  // 10. Finally, rebuild the table using the filtered data
+buildTable(filterTable);
+};
+
+// 2. Attach an event to listen for changes to each filter
+d3.selectAll("input").on("change",updateFilters);
+
+// Build the table when the page loads
+buildTable(tableData);
